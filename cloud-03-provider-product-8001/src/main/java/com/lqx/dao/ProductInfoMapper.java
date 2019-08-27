e3f140d577ee479d4cdf08e537f1ccade4518560
liuqixiang <654737000@qq.com>
2019-08-27 22:53:43 +0800

    添加基本方法

--- cloud-03-provider-product-8001/src/main/java/com/lqx/dao/ProductInfoMapper.java
+++ cloud-03-provider-product-8001/src/main/java/com/lqx/dao/ProductInfoMapper.java
@@ -3,7 +3,15 @@ package com.lqx.dao;
 import com.lqx.entity.ProductInfo;
 
 public interface ProductInfoMapper {
+    int deleteByPrimaryKey(String productId);
+
     int insert(ProductInfo record);
 
     int insertSelective(ProductInfo record);
+
+    ProductInfo selectByPrimaryKey(String productId);
+
+    int updateByPrimaryKeySelective(ProductInfo record);
+
+    int updateByPrimaryKey(ProductInfo record);
 }
